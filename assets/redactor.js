111a244a6126e8a63bc2416d698d0f6f6435a402
Stuart Millington <stuart.millington@oaksoftware.co.uk>
2022-05-22 15:21:16 +0100

    pasted images dev

--- assets/redactor.js
+++ assets/redactor.js
@@ -2154,10 +2154,11 @@
                                         
                                         if (this.opts.removePastedImages === false) {
                                             //matches_array = html.match(/data\:image\/(.[^;]*);base64,(.[^"]*)\"/gi);
+                                            matches_array = html.match(/style(.[^"]*)\"/gi);
                                             //if (matches_array != null) {
                                             //    console.log(html);
                                             //}
-                                            console.log(html);
+                                            console.log(matches_array);
                                         }
 
 					if (!this.utils.isSelectAll() && typeof setMode == 'undefined')
